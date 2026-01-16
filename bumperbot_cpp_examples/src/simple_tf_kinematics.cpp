c87695b02573b67ea2603fb870952ad2866da90e
Adinath <adinathkale01@gmail.com>
2026-01-16 23:15:17 +0530

    service server, service client & Simple Tf2 listener

--- bumperbot_cpp_examples/src/simple_tf_kinematics.cpp
+++ bumperbot_cpp_examples/src/simple_tf_kinematics.cpp
@@ -2,6 +2,8 @@
 
 using namespace std::chrono_literals;
 
+using namespace std::placeholders;
+
 
 
 SimpleTfKinematics::SimpleTfKinematics(const std::string &name)
@@ -14,6 +16,10 @@ SimpleTfKinematics::SimpleTfKinematics(const std::string &name)
    static_tf_broadcaster_=std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
 
    dynamic_tf_broadcaster_=std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+   tf_buffer_=std::make_unique<tf2_ros::Buffer>(get_clock());
+   tf_listener_=std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
 
    static_transform_stamped_.header.stamp = get_clock()->now();
    static_transform_stamped_.header.frame_id = "bumperbot_base";
@@ -33,6 +39,10 @@ SimpleTfKinematics::SimpleTfKinematics(const std::string &name)
 
    timer_ = create_wall_timer(0.1s, std::bind(&SimpleTfKinematics::timerCallback, this));
 
+   get_transform_srv_ = create_service<bumperbot_msgs::srv::GetTransform>("get_transform", 
+      std::bind(&SimpleTfKinematics::getTransformCallback, this, _1,_2));
+
+   
 }
 
 void SimpleTfKinematics::timerCallback()
@@ -52,7 +62,28 @@ void SimpleTfKinematics::timerCallback()
    last_x_ = dynamic_transform_stamped_.transform.translation.x;
 
 
+}
 
+
+bool SimpleTfKinematics::getTransformCallback(const std::shared_ptr<bumperbot_msgs::srv::GetTransform::Request> req,
+                                             const std::shared_ptr<bumperbot_msgs::srv::GetTransform::Response> res)
+
+{
+
+  RCLCPP_INFO_STREAM(get_logger(), "Requested Transform between " << req->frame_id << " and " << req->child_frame_id);
+  geometry_msgs::msg::TransformStamped requested_transform;
+  try{
+      requested_transform = tf_buffer_->lookupTransform(req->frame_id, req->child_frame_id, tf2::TimePointZero);
+  }
+  catch(tf2::TransformException &ex)
+  {
+   RCLCPP_ERROR_STREAM(get_logger(), "An error occured while transforming from : " << req->frame_id << " and " << req->child_frame_id << ": " << ex.what());
+   res->success = false;
+   return true;
+  }
+  res->transform = requested_transform;
+  res->success = true;
+  return true;
 }
 
 int main(int argc, char* argv[])
